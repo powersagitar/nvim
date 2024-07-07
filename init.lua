9cd9671b2183633f4f78bedd8e1d4c97c1d6c41b
powersagitar <114868133+powersagitar@users.noreply.github.com>
2024-07-07 16:45:40 +0800

    feat: add custom lazyvim config

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- load vimrc
+-- important: load vimrc after LazyVim to avoid conflicts
+vim.cmd.source(vim.env.HOME .. "/.vimrc")
